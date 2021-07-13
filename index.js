be192f54f3abe08353e79c44bd198b8f405e0b3e
Sriram N V <55707509+sriramNV@users.noreply.github.com>
2021-07-13 20:09:13 +0530

    added product review, product offers and searchin products functions to the api

--- index.js
+++ index.js
@@ -27,4 +27,46 @@ app.get('/products/:productId', async(req, res)=>{
     }
 });
 
+// get product reviews
+app.get('/products/:productId/reviews', async(req, res)=>{
+    const { productId } = req.params;
+
+    try{
+        const response = await request(`${baseURL}&url=https://www.amazon.com/product-reviews/${productId}`);
+
+        res.json(JSON.parse(response));
+
+    }catch(error){
+        res.json(error);
+    }
+});
+
+// get product offers
+app.get('/products/:productId/offers', async(req, res)=>{
+    const { productId } = req.params;
+
+    try{
+        const response = await request(`${baseURL}&url=https://www.amazon.com/gp/offer-listing/${productId}`);
+
+        res.json(JSON.parse(response));
+
+    }catch(error){
+        res.json(error);
+    }
+});
+
+// get search results
+app.get('/search/:searchQuery', async(req, res)=>{
+    const { searchQuery } = req.params;
+
+    try{
+        const response = await request(`${baseURL}&url=https://www.amazon.com/s?k=${searchQuery}`);
+
+        res.json(JSON.parse(response));
+
+    }catch(error){
+        res.json(error);
+    }
+});
+
 app.listen(PORT, ()=>console.log(`server running on port ${PORT}`));
